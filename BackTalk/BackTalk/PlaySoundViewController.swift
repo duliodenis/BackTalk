61c98d1d7bf261abc2369ae643d0fc57705eee29
Dulio Denis <dulio.denis@yahoo.com>
2015-03-25 17:42:52 -0400

    Added Audio Playback Effects with Pitch distortions

--- BackTalk/BackTalk/PlaySoundViewController.swift
+++ BackTalk/BackTalk/PlaySoundViewController.swift
@@ -14,10 +14,16 @@ class PlaySoundViewController: UIViewController {
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathURL, error: nil)
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathURL, error: nil)
     }
 
     @IBAction func slowSound(sender: UIButton) {
@@ -32,8 +38,38 @@ class PlaySoundViewController: UIViewController {
         audioPlayer.play()
     }
     
+    @IBAction func highPitchSound(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
+    @IBAction func vaderSound(sender: AnyObject) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
     @IBAction func stopSound(sender: UIButton) {
         audioPlayer.stop()
         audioPlayer.currentTime = 0.0
     }
+    
 }
